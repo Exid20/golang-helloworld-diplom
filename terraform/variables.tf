d1f1f846aaa0b55260c815f15a590a7c114a2f4c
Exid <exid@exid.lan>
2025-07-23 23:01:15 +0300

    test 3

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,5 +4,4 @@ variable "oauth_token" {}
 variable "zone" { default = "ru-central1-a" }
 variable "image_id" { default = "fd8u1vhsbjt0g2g4fjit" } # Ubuntu 22.04 LTS, можно обновить при необходимости
 variable "subnet_id" {}
-variable "ssh_public_key_path" {}
 variable "ssh_public_key" {}
